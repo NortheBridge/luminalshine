738bb4e34ee39eec46fcc9c9acc60b6d39cdcb12
NortheBridge Foundation Docs Publisher <noreply@northebridge.com>
2026-05-10 19:36:29 +0000

    Deploy site from b15719187ad2655b0f1284414f0e7c75fcdd7094

--- src/platform/windows/service_constants.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace platf::service_launch {
-  inline constexpr wchar_t launched_by_service_env_var[] = L"SUNSHINE_LAUNCHED_BY_SERVICE";
-}
--- tools/playnite_launcher/launcher.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace playnite_launcher {
-
-  int launcher_run(int argc, char **argv);
-
-}  // namespace playnite_launcher
